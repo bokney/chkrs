3fa86a9d0bd4c6ac2d6715ad09b4780702e0d201
bokney <toknyk@gmail.com>
2024-08-31 15:13:19 +0100

    game data interface

--- src/game_states/game/game_data.h
+++ src/game_states/game/game_data.h
@@ -1,9 +1,20 @@
 
 #include <gb/gb.h>
 
+
+typedef enum _peonState {
+    P1MAN = 0,
+    P1KING = 1,
+    P1CAPTURED = 2,
+    P2MAN = 3,
+    P2KING = 4,
+    P2CAPTURED = 5
+} peonState;
+
+
 typedef struct _peonData {
 
-    uint8_t state;
+    peonState state;
     uint8_t x, y;
 
 } peonData;
@@ -11,18 +22,18 @@ typedef struct _peonData {
 
 typedef struct _gameData {
 
-    uint8_t cursor_x, cursor_y;
-    uint8_t cursor_cooldown;
-    peonData *cursor_target;
-
-    uint8_t move_counter;
-    uint8_t player_score[2];
+    uint8_t turn;
+    uint8_t score[2];
 
     peonData *board[8][8];
     peonData peons[2][12];
 
+    uint8_t cursor_x, cursor_y;
+    uint8_t cursor_cooldown;
+    peonData *cursor_target;
+
+    uint16_t move_counter;
+
 } gameData;
 
-typedef struct _captureData {
-    uint8_t count;
-} captureData;
+extern gameData game_data;
